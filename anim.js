2c07deecd6546d3783965067c90dc895f15ba828
PC <cristiancamilocabreragarcia@gmail.com>
2024-09-21 18:46:33 -0500

    first commit

---
+++ anim.js
@@ -0,0 +1,72 @@
+// Sincronizar las letras con la canción
+var audio = document.querySelector("audio");
+var lyrics = document.querySelector("#lyrics");
+
+// Array de objetos que contiene cada línea y su tiempo de aparición en segundos
+var lyricsData = [
+  { text: "Nos dimos todo los que se nos dio,", time: 21 },
+  { text: "Nos dimos todo eso y mucho más", time: 26 },
+  { text: "Para despues reconocernos otra vez", time: 32 },
+  { text: "Y nos damos todo lo que se nos da,", time: 40.9 },
+  { text: "Nos damos todo es y mucho más", time: 46 },
+  { text: "Amanecer colgado de tus labios", time: 51 },
+  { text: "¡Oh!", time: 58 },
+  { text: "Brillas", time: 54 },
+  { text: "Y brillas tan lindo", time: 61 },
+  { text: "Y brillamos juntos", time: 63.4 },
+  { text: "Entre Pestañas", time: 66 },
+  { text: "Divina,", time: 69 },
+  { text: "Divina sonrisa", time: 71 },
+  { text: "Abrazo de luna,", time: 74 },
+  { text: "De luna llena", time: 76.8 },
+  { text: "Too much light for twilight", time: 104 },
+  { text: "In the mood for the flowers love", time: 108 },
+  { text: "At the time", time: 144 },
+  { text: "The whisper of birds", time: 148 },
+  { text: "Lonely before the sun cried", time: 153 },
+  { text: "Fell from the sky", time: 158 },
+  { text: "Like water drops", time: 164 },
+  { text: "Where I'm now? I don't know why", time: 169 },
+  { text: "Nice butterflies in my hands", time: 176 },
+  { text: "Too much light for twilight", time: 183 },
+  { text: "In the mood for the flowers", time: 188 },
+  { text: "Love.", time: 140 },
+];
+
+// Animar las letras
+function updateLyrics() {
+  var time = Math.floor(audio.currentTime);
+  var currentLine = lyricsData.find(
+    (line) => time >= line.time && time < line.time + 6
+  );
+
+  if (currentLine) {
+    // Calcula la opacidad basada en el tiempo en la línea actual
+    var fadeInDuration = 0.1; // Duración del efecto de aparición en segundos
+    var opacity = Math.min(1, (time - currentLine.time) / fadeInDuration);
+
+    // Aplica el efecto de aparición
+    lyrics.style.opacity = opacity;
+    lyrics.innerHTML = currentLine.text;
+  } else {
+    // Restablece la opacidad y el contenido si no hay una línea actual
+    lyrics.style.opacity = 0;
+    lyrics.innerHTML = "";
+  }
+}
+
+setInterval(updateLyrics, 1000);
+
+//funcion titulo
+// Función para ocultar el título después de 216 segundos
+function ocultarTitulo() {
+  var titulo = document.querySelector(".titulo");
+  titulo.style.animation =
+    "fadeOut 3s ease-in-out forwards"; /* Duración y función de temporización de la desaparición */
+  setTimeout(function () {
+    titulo.style.display = "none";
+  }, 3000); // Espera 3 segundos antes de ocultar completamente
+}
+
+// Llama a la función después de 216 segundos (216,000 milisegundos)
+setTimeout(ocultarTitulo, 216000);
